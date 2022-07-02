9371bae6f15e8907c7fe7870823c58201d3ab8db
Muskaan <muskaan19010@gmail.com>
2022-07-02 19:31:29 +0530

    feat: Create API call for user-signup

---
+++ src/pages/api/signup.ts
@@ -0,0 +1,52 @@
+import { NextApiRequest, NextApiResponse } from "next";
+import jwt from "jsonwebtoken";
+import bcrypt from "bcrypt";
+import cookie from "cookie";
+
+import prisma from "../../../lib/prismaClient";
+
+export default async (req: NextApiRequest, res: NextApiResponse) => {
+  // todo: check if it's a POST request
+  const salt = bcrypt.genSaltSync();
+  const { email, password, firstName, lastName } = req.body;
+
+  let user;
+
+  try {
+    user = await prisma.user.create({
+      data: {
+        firstName,
+        lastName,
+        email,
+        password: bcrypt.hashSync(password, salt),
+      },
+    });
+  } catch (e) {
+    res.status(401);
+    res.json({ error: "User already exists" });
+    return;
+  }
+
+  const token = jwt.sign(
+    {
+      email: user.email,
+      id: user.id,
+      time: Date.now(),
+    },
+    "hello-test",
+    { expiresIn: "8h" }
+  );
+
+  res.setHeader(
+    "Set-Cookie",
+    cookie.serialize("ACCESS_TOKEN", token, {
+      httpOnly: true,
+      maxAge: 8 * 60 * 60,
+      path: "/",
+      sameSite: "lax",
+      secure: process.env.NODE_ENV === "production",
+    })
+  );
+
+  res.json(user);
+};
