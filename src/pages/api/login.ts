fa7e18bcc25e642d4a0bc81aa4fa5a237435072d
Muskaan <muskaan19010@gmail.com>
2022-07-03 14:13:39 +0530

    feat: Created new file-login.ts that contains Login API logic, deleted hello.ts file in /pages dir

---
+++ src/pages/api/login.ts
@@ -0,0 +1,39 @@
+import { NextApiRequest, NextApiResponse } from "next";
+import jwt from "jsonwebtoken";
+import bcrypt from "bcrypt";
+import cookie from "cookie";
+
+import prisma from "../../../lib/prismaClient";
+
+export default async (req: NextApiRequest, res: NextApiResponse) => {
+  const { email, password } = req.body;
+
+  const user = await prisma.user.findUnique({ where: { email } });
+
+  if (user && bcrypt.compareSync(password, user.password)) {
+    const token = jwt.sign(
+      {
+        id: user.id,
+        email: user.email,
+        time: Date.now(), // todo :find use for Date here?
+      },
+      process.env.JWT_SECRET_KEY || "hello-test",
+      { expiresIn: "8h" }
+    );
+
+    res.setHeader(
+      "Set-Cookie",
+      cookie.serialize("ACCESS_TOKEN", token, {
+        httpOnly: true,
+        maxAge: 8 * 60 * 60,
+        path: "/",
+        sameSite: "lax",
+        secure: process.env.NODE_ENV === "production",
+      })
+    );
+    res.json(user);
+  } else {
+    res.status(401);
+    res.json({ error: "Invalid Email or Password." });
+  }
+};
