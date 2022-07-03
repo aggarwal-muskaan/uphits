6a66e51cd84f672f477b4f028e6c16eec9f72363
Muskaan <muskaan19010@gmail.com>
2022-07-03 14:14:41 +0530

    feat: New file-fetcher.ts to send API requests on same domain & throw errors if any

---
+++ src/lib/fetcher.ts
@@ -0,0 +1,17 @@
+const fetcher = async (url: string, data = undefined) => {
+  return fetch(`${window.location.origin}/api${url}`, {
+    method: data ? "POST" : "GET",
+    credentials: "include",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(data),
+  }).then((res) => {
+    if (res.status > 399 && res.status < 200) {
+      throw new Error();
+    }
+    return res.json();
+  });
+};
+
+export default fetcher;
