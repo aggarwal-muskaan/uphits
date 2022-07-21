866061f2e1e6b4294f4d72ca570557cba15152bd
Muskaan <muskaan19010@gmail.com>
2022-07-21 11:52:19 +0530

    feat: Add required validation on each field on Signup form
    All fields are required
    Email should match a pattern
    Email and confirm email should match
    Call submitForm function when signup-form if submit

--- src/pages/signup.tsx
+++ src/pages/signup.tsx
@@ -1,22 +1,51 @@
+import React, { useEffect } from "react";
 import Link from "next/link";
+import { useRouter } from "next/router";
 import { useForm } from "react-hook-form";
 import { Flex, Heading, Text } from "@chakra-ui/layout";
 
 import AuthTemplate from "../components/AuthTemplate";
 import ControlledInputField from "../components/shared/ControlledInputField";
 import { FilledAuthButton } from "../components/shared/Button";
+import { EMAIL, TRIM_STRING } from "../lib/regex";
 import { TSignup } from "../types";
 
 function Signup() {
-  const { control } = useForm<TSignup>({
-    defaultValues: {
-      email: "",
-      confirmEmail: "",
-      newPassword: "",
-      profileName: "",
-    },
-    mode: "all",
-  });
+  const [isLoading, setIsLoading] = React.useState(false);
+  const router = useRouter();
+
+  const { control, handleSubmit, watch, setError, clearErrors } =
+    useForm<TSignup>({
+      defaultValues: {
+        email: "",
+        confirmEmail: "",
+        newPassword: "",
+        profileName: "",
+      },
+      mode: "all",
+    });
+  const email = watch("email");
+  const reEmail = watch("confirmEmail");
+
+  useEffect(() => {
+    if (reEmail.length !== 0 && email !== reEmail)
+      setError("confirmEmail", {
+        type: "value",
+        message: "Email does not match",
+      });
+    else if (reEmail.length !== 0 && email === reEmail)
+      clearErrors("confirmEmail");
+  }, [email]);
+
+  const submitForm = () => {
+    // e.preventDefault();
+    setIsLoading(true);
+
+    // await auth(data);
+    setIsLoading(false);
+    alert("oops!!");
+    // router.push("/");
+  };
 
   const signupForm = (
     <>
@@ -29,20 +58,26 @@ function Signup() {
       >
         Sign up for free to start listening.
       </Heading>
-      <form>
+      <form onSubmit={handleSubmit(submitForm)}>
         <ControlledInputField
           name="email"
           control={control}
           label="What's your email?"
           placeholder="Enter your email."
-          rules={{ required: "You need to enter your email." }}
+          rules={{
+            required: "You need to enter your email.",
+            pattern: { value: EMAIL, message: "Invalid Email" },
+          }}
         />
         <ControlledInputField
           name="confirmEmail"
           control={control}
           label="Confirm your email?"
           placeholder="Enter your email again."
-          rules={{ required: "You need to confirm your email." }}
+          rules={{
+            required: "You need to confirm your email.",
+            validate: (reEmail) => reEmail === email || "Email does not match",
+          }}
         />
         <ControlledInputField
           name="newPassword"
@@ -50,17 +85,29 @@ function Signup() {
           label="Create a password"
           placeholder="Create a password."
           typeOfInput="password"
-          rules={{ required: "You need to enter a password." }}
+          rules={{
+            required: "You need to enter a password.",
+          }}
         />
         <ControlledInputField
           name="profileName"
           control={control}
           label="What should we call you?"
           placeholder="Enter a profile name."
-          rules={{ required: "Enter a name for your profile." }}
+          rules={{
+            required: "Enter a name for your profile.",
+            pattern: {
+              value: TRIM_STRING,
+              message: "Profile name must start with a letter",
+            },
+          }}
         />
         <Flex justifyContent="center">
-          <FilledAuthButton text="Sign up" />
+          <FilledAuthButton
+            text="Sign up"
+            buttonType="submit"
+            loadingState={isLoading}
+          />
         </Flex>
       </form>
       <Text
