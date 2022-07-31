a2175777274b1d87b52074a2c1c4bbc48b852c3b
Muskaan <muskaan19010@gmail.com>
2022-07-31 22:20:21 +0530

    feat: Create new file that consists all util functions
    Added getBgColor() that takes id as param and return random color

---
+++ src/lib/utilFunctions.ts
@@ -0,0 +1,14 @@
+export const getBgColor = (id: number) => {
+  const colors = [
+    "red",
+    "green",
+    "blue",
+    "orange",
+    "purple",
+    "gray",
+    "teal",
+    "yellow",
+  ];
+
+  return colors[id - 1] || colors[Math.floor(Math.random() * colors.length)];
+};
