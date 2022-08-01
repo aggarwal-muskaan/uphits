7625574098295485c5c9a67598bd89dcf9adaa8a
Muskaan <muskaan19010@gmail.com>
2022-08-01 11:53:58 +0530

    feat: Add formatters in util file

--- src/lib/utilFunctions.ts
+++ src/lib/utilFunctions.ts
@@ -1,3 +1,5 @@
+import formatDuration from "format-duration";
+
 export const getBgColor = (id: number) => {
   const colors = [
     "red",
@@ -11,4 +13,16 @@ export const getBgColor = (id: number) => {
   ];
 
   return colors[id - 1] || colors[Math.floor(Math.random() * colors.length)];
+};
+
+export const formatTime = (timeInSeconds = 0) => {
+  return formatDuration(timeInSeconds * 1000);
+};
+
+export const formatDate = (date: Date) => {
+  return date.toLocaleDateString("en-US", {
+    year: "numeric",
+    month: "short",
+    day: "numeric",
+  });
 };
