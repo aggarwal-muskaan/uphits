8a16f10e9eb727402fdcacd62cfe0b3feef7400e
Muskaan <muskaan19010@gmail.com>
2022-08-01 20:13:19 +0530

    refactor: Modify condition in _middleware.ts file

--- src/pages/_middleware.ts
+++ src/pages/_middleware.ts
@@ -5,14 +5,15 @@
 import { NextRequest, NextResponse } from "next/server";
 
 const signedInPages = ["/", "/playlist", "/mylibrary"]; // once you're authorised, you've access to all these routes
+const authPages = ["/login", "/signup"];
 
 export const middleware = (req: NextRequest) => {
+  const accessToken = req.cookies.ACCESS_TOKEN;
   if (signedInPages.find((p) => p === req.nextUrl.pathname)) {
-    const accessToken = req.cookies.ACCESS_TOKEN;
-
     if (!accessToken) {
       return NextResponse.redirect("/login");
       // return NextResponse.redirect(new URL("/login", req.url));
     }
-  }
+  } else if (authPages.includes(req.nextUrl.pathname) && accessToken)
+    return NextResponse.redirect("/"); // don't show login or signup page if you're valid logged-in user
 };
