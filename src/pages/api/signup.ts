64bf04e3ccffade9ec3ae59049f597e91b157833
Muskaan <muskaan19010@gmail.com>
2022-08-03 18:35:36 +0530

    chore: Increased life of access-token from 8hours to 36hours

--- src/pages/api/signup.ts
+++ src/pages/api/signup.ts
@@ -51,7 +51,7 @@ export default async (req: NextApiRequest, res: NextApiResponse) => {
       "Set-Cookie",
       cookie.serialize("ACCESS_TOKEN", token, {
         httpOnly: true,
-        maxAge: 8 * 60 * 60,
+        maxAge: 36 * 60 * 60,
         path: "/",
         sameSite: "lax",
         secure: process.env.NODE_ENV === "production",
