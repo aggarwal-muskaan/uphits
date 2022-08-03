a7ad06d0fb875601b965136b3289aff8bafcb461
Muskaan <muskaan19010@gmail.com>
2022-08-03 18:32:50 +0530

    feat: Created a store file that contains global state of the app
    Passes store context to _app.tsx with the help of StoreProvider component

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,19 +2,24 @@
 import "reset-css";
 // pages/_app.js
 import { ChakraProvider } from "@chakra-ui/react";
-import { theme } from "../lib/theme";
+
+import { StoreProvider } from "easy-peasy";
 import PlayerTemplate from "../components/PlayerTemplate";
+import { theme } from "../lib/theme";
+import { store } from "../lib/store";
 
 function MyApp({ Component, pageProps }) {
   return (
     <ChakraProvider theme={theme} resetCSS>
-      {Component.authPage ? (
-        <Component {...pageProps} />
-      ) : (
-        <PlayerTemplate>
+      <StoreProvider store={store}>
+        {Component.authPage ? (
           <Component {...pageProps} />
-        </PlayerTemplate>
-      )}
+        ) : (
+          <PlayerTemplate>
+            <Component {...pageProps} />
+          </PlayerTemplate>
+        )}
+      </StoreProvider>
     </ChakraProvider>
   );
 }
