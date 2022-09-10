222955d5efdc71a00843e0164418fdb44131eedf
Muskaan <muskaan19010@gmail.com>
2022-09-10 13:50:14 +0530

    feat: Modify state to play song when clicked on button in a playlist-table

--- src/components/music/SongTable.tsx
+++ src/components/music/SongTable.tsx
@@ -2,10 +2,19 @@ import { Table, Thead, Td, Tr, Tbody, Th, IconButton } from "@chakra-ui/react";
 import { Box } from "@chakra-ui/layout";
 import { AiOutlineClockCircle } from "react-icons/ai";
 import { BsFillPlayFill } from "react-icons/bs";
+import { useStoreActions } from "easy-peasy";
 
 import { formatDate, formatTime } from "../../lib/utilFunctions";
 
 export const SongsTable = ({ songs }) => {
+  const playSongs = useStoreActions((store: any) => store.changeActiveSongs);
+  const setActiveSong = useStoreActions((store: any) => store.changeActiveSong);
+
+  const handlePlay = (activeSong?) => {
+    setActiveSong(activeSong || songs[0]);
+    playSongs(songs);
+  };
+
   return (
     <Box bg="transparent" color="white">
       <Box padding="10px" marginBottom="20px">
@@ -16,6 +25,7 @@ export const SongsTable = ({ songs }) => {
             aria-label="play"
             size="lg"
             isRound
+            onClick={() => handlePlay()}
           />
         </Box>
         <Table variant="unstyled">
