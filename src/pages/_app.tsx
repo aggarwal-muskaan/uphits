d0d54ee380f6c52e4bebb4031ea9aa7840f16066
Muskaan <muskaan19010@gmail.com>
2022-11-20 17:20:45 +0530

    feat: Typechecked custom prop in _app.tsx children

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -7,8 +7,14 @@ import { StoreProvider } from "easy-peasy";
 import PlayerTemplate from "../components/PlayerTemplate";
 import { theme } from "../lib/theme";
 import { store } from "../lib/store";
+import { AppProps } from "next/app";
+import { NextComponentType } from "next";
 
-function MyApp({ Component, pageProps }) {
+type CustomAppProps = AppProps & {
+  Component: NextComponentType & { authPage?: boolean }; // add auth type
+};
+
+function MyApp({ Component, pageProps }: CustomAppProps) {
   return (
     <ChakraProvider theme={theme} resetCSS>
       <StoreProvider store={store}>
